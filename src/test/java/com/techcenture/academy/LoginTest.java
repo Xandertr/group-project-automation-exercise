631e584f1207a436acebeb27a83d5be22e229d3f
Yakov <yashag@gmail.com>
2023-06-01 14:25:00 -0400

    Yakov update

--- src/test/java/com/techcenture/academy/LoginTest.java
+++ src/test/java/com/techcenture/academy/LoginTest.java
@@ -1,5 +1,6 @@
 package com.techcenture.academy;
 
+import com.relevantcodes.extentreports.LogStatus;
 import com.techcenture.academy.pages.HomePage;
 import com.techcenture.academy.pages.RegisterPage;
 import org.testng.annotations.Test;
@@ -11,8 +12,10 @@ public class LoginTest extends BaseTest {
     private RegisterPage registerPage;
     @Test
     public void loginTest(){
+        extentTest = extentReports.startTest("login positive test");
         homePage =new HomePage(driver);
         homePage.navigateToUrl();
+        extentTest.log(LogStatus.INFO, "initiate web-driver successfully");
         homePage.VerifyTitle();
         homePage.clickLogin();
         homePage.verifyNewUserSignup();
